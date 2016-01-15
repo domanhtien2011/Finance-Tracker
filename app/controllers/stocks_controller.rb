f4566ef163e5b218101355ee916dacf598c49f0d
domanhtien2011 <domanhtien2011@gmail.com>
2016-01-15 22:03:57 +0700

    Add friendship and search functionality

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,17 +1,12 @@
 class StocksController < ApplicationController
+
   def search
-    # Check the data (the name of the stock) that is sent to the controller search
     if params[:stock]
-      # Search the stock to see whether it is already in the database or not.
       @stock = Stock.find_by_ticker(params[:stock])
-      # If the stock is not in the database then search online by using the methods from the gem stock_quote
       @stock ||= Stock.new_from_lookup(params[:stock])
     end
 
-    # Next action after search for the stock
     if @stock
-      # Use this trick to test whether my method can search for a stock or not by typing localhost:3000/search_stocks?stock=GOOG
-      # render json: @stock
       render partial: 'lookup'
     else
       render status: :not_found, nothing: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,30 +1,68 @@
+# Model for User
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # :recoverable
+  devise :database_authenticatable, :registerable,
+         :rememberable, :trackable, :validatable
+
   has_many :user_stocks
   has_many :stocks, through: :user_stocks
   has_many :friendships
   has_many :friends, through: :friendships
 
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  class << self
+    def search(param)
+      return user.none if param.blank?
 
-  def can_add_stock?(ticker_symbol)
-    under_stock_limit? && !stock_already_added?(ticker_symbol)
-  end
+      param.strip!
+      param.downcase!
 
-  def under_stock_limit?
-    (user_stocks.count < 10)
-  end
+      (first_name_matches(param) + last_name_matches(param) + email_matches(param)).uniq
+    end
 
-  def stock_already_added?(ticker_symbol)
-    stock = Stock.find_by_ticker(ticker_symbol)
-    return false unless stock
-    user_stocks.where(stock_id: stock.id).exists?
+    def first_name_matches(param)
+      matches('first_name', param)
+    end
+
+    def last_name_matches(param)
+      matches('last_name', param)
+    end
+
+    def email_matches(param)
+      matches('email', param)
+    end
+
+    def matches(field_name, param)
+      where("LOWER(#{field_name}) LIKE ?", "%#{param}%")
+    end
   end
 
   def full_name
-    return "#{first_name} #{last_name}".strip if (first_name || last_name)
-    "Anonymous"
+    "#{first_name} #{last_name}".strip
+  end
+
+  def can_add_stock?(ticker)
+    under_stock_limit? &&
+      !stock_already_added?(ticker)
+  end
+
+  def under_stock_limit?
+    user_stocks.count < 10
+  end
+
+  def stock_already_added?(ticker)
+    stock = Stock.find_by_ticker ticker
+    return false unless stock
+
+    user_stocks.where(stock_id: stock.id).exists?
+  end
+
+  def not_friends_with?(other)
+    friendships.where(friend_id: other.id).count < 1
+  end
+
+  def except_current_user(users)
+    users.reject { |user| user.id == self.id }
   end
 end
