5bf961d94b3a58b4fc4712744912cedc0bed8502
domanhtien2011 <domanhtien2011@gmail.com>
2016-01-15 22:30:05 +0700

    Add destroy friendship and show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,12 @@
 # Controller fur users
 class UsersController < ApplicationController
   def show
-    @user        = User.find params[:id]
+    @user = User.find(params[:id])
     @user_stocks = @user.stocks
   end
 
   def my_portfolio
-    @user        = current_user
+    @user = current_user
     @user_stocks = @user.stocks
   end
 
